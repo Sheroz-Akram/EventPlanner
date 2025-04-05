20fc7458c6ff04269b9c29fe127f57cd32330b1e
Sheroz Akram <sheroz.akram@outlook.com>
2025-04-05 15:28:41 +0500

    Add Ping Controller

---
+++ api/EventPlanner.WebAPI/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventPlanner.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Api is working");
+        }
+    }
+}
